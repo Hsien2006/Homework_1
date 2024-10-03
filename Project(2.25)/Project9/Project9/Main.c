f80d23b57eea7d49ffde633d30fff810e38e64c0
Hsien2006 <t113360216@ntut.org.tw>
2024-10-03 16:46:02 +0800

    Add files via upload

---
+++ Project(2.25)/Project9/Project9/Main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	printf("W   W   W\n");
+	printf(" W  W  W\n");
+	printf("  W W W\n");
+	printf("   W W\n\n");
+
+	printf("PPPPPPPPP\n");
+	printf("    P   P\n");
+	printf("    P   P\n");
+	printf("     P P\n\n");
+
+	printf("HHHHHHHHH\n");
+	printf("    H    \n");
+	printf("    H    \n");
+	printf("HHHHHHHHH\n");
+
+
+}
